3b34c047e58eb3eed8e38d2381e5b680397fe718
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-02-06 13:17:01 -0500

    Add release script, update package.json

---
+++ release.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+set -e
+# Variables
+
+echo "Welcome to HackMcGill's HackerAPI-client Deployment Script!"
+echo "==================================================="
+
+if [ ! ${BRANCH} = "master" ]; then
+    echo "Current branch: ${BRANCH}"
+    echo "ERROR: Release operation is only available on master branch."
+    echo "Deployment failed. Exiting."
+    exit 1
+fi
+echo "Release modes:"
+PS3='Please select one of the above release version modes:'
+options=("Patch" "Minor" "Major" "Quit")
+select opt in "${options[@]}"
+do
+    case ${opt} in
+	"Patch")
+	    MODE=patch
+	    break
+	    ;;
+	"Minor")
+	    MODE=minor
+	    break
+	    ;;
+	"Major")
+	    MODE=major
+	    break
+	    ;;
+	"Quit")
+	    echo "Deployment cancelled. Exiting."
+	    exit 1
+	    ;;
+	*) echo "Invalid option";;
+    esac
+done
+
+# Update current branch
+git pull origin master
+
+# Compile the code
+echo "=============="
+echo "Compiling Code"
+echo "=============="
+tsc
+
+echo "Code compiled!"
+
+echo "=============="
+echo "Update Version"
+echo "=============="
+npm version ${MODE}
+version=$(npm run version --silent)
+
+echo "=============="
+echo "Tagging Github"
+echo "=============="
+# Tag Github
+git add -A
+git commit -m "version ${version}"
+git tag -a "${version}" -m "version ${version}"
+git push origin master
+git push origin master --tags
+
+echo "================="
+echo "Publishig via NPM"
+echo "================="
+# Publish
+npm publish
