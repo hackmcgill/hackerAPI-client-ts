70d0e6d3e598acfb754ace0a36d8bad5e685dcb9
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-02-06 13:19:56 -0500

    Update release script

--- release.sh
+++ release.sh
@@ -60,8 +60,6 @@ echo "=============="
 echo "Tagging Github"
 echo "=============="
 # Tag Github
-git add -A
-git commit -m "version ${version}"
 git tag -a "${version}" -m "version ${version}"
 git push origin master
 git push origin master --tags
